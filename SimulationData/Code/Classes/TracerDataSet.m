c736acc58c057e598fac733bfca0012a05de16cc
zak <uphgreat@gmail.com>
2014-02-19 03:35:04 -0800

    Added TracerDataSet class and EllipToMat.m
    TracerDataSet is designed to hold the data from simulations for
    one charge and one quip direction.
    
    EllipToMat.m is designed to take a collected output file from the
    server and turn it into a sorted .mat file that the
    PositionGenerator class will be able to use once it is updated.

---
+++ SimulationData/Code/Classes/TracerDataSet.m
@@ -0,0 +1,67 @@
+classdef TracerDataSet < handle
+    %Stores the data from one tracer run (one charge and one quip).
+    
+    properties (Constant)
+        TRACER_DATA_DIR='../../TracerOutput';
+        TRACER_REGEX='SimData_([pn])(\d+)[eE](\d+)([LR])';
+    end
+    
+    
+    properties (SetAccess=private)
+        charge %Fractional charge given
+        quip %direction, either 'left' or 'right'
+        z_positions %z-positions of data
+    end
+    
+    methods
+        
+        function self = TracerDataSet(file_name)
+            %Initializes the TracerDataSet instance.  file_name should the
+            %name of a file in the TracerOutput directory (without path).
+            self.interpret_file_name(file_name);
+            full_file_name=fullfile(TracerDataSet.TRACER_DATA_DIR,file_name);
+            self.z_positions=load_mat(full_file_name);
+        end
+        
+    end
+    
+    methods (Hidden)
+        
+        function [] = interpret_file_name(self,file_name)
+            %Parses the file name to set charge and quip
+            
+            match=regexp(file_name,TracerDataSet.TRACER_REGEX,'tokens');
+            match=match{1};
+            if isempty(match)
+                msgIdent='TracerDataSet:interpret_file_name:NameError';
+                msgString='Invalid file_name %s\nfile_name must be ';
+                msgString=[msgString,'something like p4e8L'];
+                error(msgIdent,msgString,file_name);
+            end
+            
+            %Figure out charge
+            sign_char=match{1};
+            if sign_char=='n'
+                sign=-1;
+            elseif sign_char=='p'
+                sign=1;
+            end
+            
+            mantissa=str2double(match{2});
+            exponent=str2double(match{3});
+            self.charge=sign*mantissa*10^(-exponent);
+            
+            %Figure out quip direction
+            quip_char=match{4};
+            if quip_char=='L'
+                self.quip='left';
+            elseif quip_char=='R'
+                self.quip='right';
+            end
+            
+        end
+        
+    end
+    
+end
+
---
+++ SimulationData/Code/OtherFunctions/EllipToMat.m
@@ -0,0 +1,12 @@
+function [] = EllipToMat(file_name)
+%Converts the given file_name into a sorted .mat file
+%   file_name should be relative to the TracerOutput directory.
+    file_name=fullfile('../../TracerOutput/',file_name);
+    [directory,bare_name,~]=fileparts(file_name);
+    out_file_name=fullfile( directory, strcat(bare_name,'.mat') );
+    t_z_array=dlmread(file_name);
+    data_array=t_z_array(:,2);
+    data_array=sortrows(data_array);
+    save_mat(out_file_name,data_array);
+    disp( strjoin({'Created',out_file_name}) );
+end
