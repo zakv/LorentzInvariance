e4a14c4e50ce53898fc659742459d6d87a27ccb4
fumikac <fumika21@gmail.com>
2014-03-04 18:43:41 -0800

    changed the out put. Now it also returns first/last event time.

--- ExperimentalTimeData/Code/TimeAnalysisFunction/calc_events_per_time_cycle.m
+++ ExperimentalTimeData/Code/TimeAnalysisFunction/calc_events_per_time_cycle.m
@@ -1,4 +1,4 @@
-function [count] = calc_events_per_time_cycle(timeCycle)
+function [count_first_last] = calc_events_per_time_cycle(timeCycle)
 %calculate successful events per shift cycle
 
 startTime = timeCycle(:,1);
@@ -11,15 +11,22 @@ iMax = numel(time);
 jMax = numel(startTime);
 count = zeros(size(startTime));
 shiftFound = zeros(iMax,1); 
-
+firstEventTime = zeros(jMax,1);
+lastEventTime = zeros(jMax,1);
+count_first_last = zeros(jMax,3);
 for j = 1:jMax
     count(j) = 0;
     for i = 1:iMax
         if startTime(j)-(1/48) <= time(i) && time(i) <= endTime(j) + (1/48) %+-30min
             count(j) = count(j) + 1;
             shiftFound(i) = true;
+            if count(j) == 1
+                firstEventTime(j) = time(i);
+            end
+            lastEventTime(j) = time(i);
         end
     end
+    count_first_last(j,:) = [count(j),firstEventTime(j),lastEventTime(j)];
 end
 
 for i = 1:iMax
