b6085f649343769b88ed7c9c83f27ad8e0aec6a0
fumikac <fumika21@gmail.com>
2014-03-19 16:54:09 -0700

    minor change

--- ExperimentalTimeData/Code/GetTimeFunction/GenerateEntryTimeWithEplusCatchData_raw.m
+++ ExperimentalTimeData/Code/GetTimeFunction/GenerateEntryTimeWithEplusCatchData_raw.m
@@ -9,6 +9,13 @@ run_raw = time_run_dataLog(:,1);
 spillLogEntryTime_raw = time_run_dataLog(:,2);
 dataLog_raw = time_run_dataLog(:,3);
 %decide the date for using
+%exclude 
+%run 21567 ( log 9504 )
+%16 oct 2010
+%
+%run 21609~21611   ( log 9553,9554,9555)
+%17 oct 2010
+
 
 %remove two test runs with e+ catch
 %run 24850(dataLog 12957) EntryTime 8.20.2011 7:57, run 24339(no datalog) Entrytime 2011.8.7 00:58 
