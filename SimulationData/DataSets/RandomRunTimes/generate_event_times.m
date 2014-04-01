68aa52f286d28cab1cdc1939dfa9c2a0f40b47bb
Fumikac <fumika21@gmail.com>
2014-03-31 17:52:08 -0700

    big change

--- SimulationData/DataSets/RandomRunTimes/generate_event_times.m
+++ SimulationData/DataSets/RandomRunTimes/generate_event_times.m
@@ -1,63 +1,195 @@
-function [ event_times,n_left ] = generate_event_times(random_generator)
-%Need to change which random number generator is used.  Need to update the
-%lambda values.
+function [eventTimes] = generate_event_times(random_generator)
+%running 10000 times:
+%number of events 391.9361+-26.3308
+%number of runs   323.484+-20.5068
+%time span        35.0812+-1.0816
+%
+%[Experiment]
+%eventN=386, runN=320, runSpan=34.8347
 
-%Returns a column vector of event times in Geneva time chosen to be Poisson
+%Returns a column vector of event times in UTC time chosen to be Poisson or
+%Gaussian.
 %distributed over the estimated times during which the experiment was
 %running.  The number of events per detection is also assumed to be
-%Poissonian (with the restriction that it is greater than or equal to one).
-%Currently all data points are assumed to be quip right.
+%Poissonian (with the restriction that it is greater than or equal to zero).
 
-%Constants (should update these values)
-n_Hbar_lambda=0.338;
-time_lambda=14.199513; %Days/event
+%METHOD
+%when shift starts to when actual run starts - poisson (exp for empirical)
+%when shift starts to when actual run ends - normal distribution
+%   -> time between first actual run and last actual run
+% decide number of successful run during the time span - poisson
+% decide time of successful run - random
+% decide number of events for each successful run - poisson
 
-run_times_obj=load('AttemptedStartingTimeCycleData.mat');
 
-run_starts_ends=run_times_obj.timeCycle;
-event_times=zeros(500,1); %Preallocate
+%-------------get experimental data-------------
+oldDir = cd('../../../ExperimentalTimeData/Code/TimeAnalysisFunction');
+%gets time information
+t = event_time();
+shiftCycle = t.shiftCycle();
+shiftCycle = st2utc_ch(shiftCycle);% to utc
+attemptedRunTime = t.attemptedSpillLogEntryTime();
+attemptedRunTime = st2utc_ch(attemptedRunTime);% to utc
+successfulEventTime = t.utc('all','all',0);
+
+%prepare data
+runs_cycle = calc_events_per_time_cycle(attemptedRunTime,shiftCycle);
+totalRunSpan = calc_total_time_of_cycles(runs_cycle(:,2:3));
+cd(oldDir);
+
+n_successfulEvents = numel(unique(successfulEventTime)); %320
+shiftStart_to_firstRun = runs_cycle(:,2) - shiftCycle(:,1);
+shiftStart_to_lastRun = runs_cycle(:,3) - shiftCycle(:,1);
+
+%--------calculates estimetes-----------------
+%Poisson process
+n_Hbar_lambda=lambda_event_number();%n_successfulEvents/n_attemptedRuns;
+n_runs_lambda=n_successfulEvents/totalRunSpan;%successful events/attempted runs
+%exponential distribution
+start_estimates_poisson = fit_shifted_poisson(shiftStart_to_firstRun);
+disp(start_estimates_poisson);
+%least square method for Gaussian
+end_estimates_simple = [mean(shiftStart_to_lastRun),std(shiftStart_to_lastRun)];
+end_estimates_gaussian = fit_gaussian(shiftStart_to_lastRun);
+disp(end_estimates_gaussian);
+%disp(end_estimates_gaussian)
+
+%------------generate event times--------------------
+eventTimes=zeros(500,1); %Preallocate
 row_index=1;
-jMax=size(run_starts_ends,1);
+jMax = numel(shiftStart_to_firstRun);
+
+test_n_runs = zeros(jMax,1);
+test_runSpan = zeros(jMax,1);
+shiftStart_to_firstRun_sim = zeros(jMax,1);
+shiftStart_to_lastRun_sim = zeros(jMax,1);
+shiftStart_to_lastRun_simple = zeros(jMax,1);
 for j=1:jMax
-    shift_start=run_starts_ends(j,1);
-    shift_end=run_starts_ends(j,2);
-    rand_val=random_generator.rand(1);
-    event_time=shift_start+get_interarrival_time(rand_val,time_lambda);
-    while event_time<shift_end
-        rand_val=random_generator.rand(1);
-        n_Hbars=get_n_Hbars(rand_val,n_Hbar_lambda);
-        %Add this to output event_times
-        event_times(row_index:row_index+n_Hbars-1)=event_time;
-        %Prepare for next iteration
-        row_index=row_index+n_Hbars;
-        rand_val=random_generator.rand(1);
-        event_time=event_time+get_interarrival_time(rand_val,time_lambda);
+    %for check
+    rand_val1 = random_generator.rand(1);
+    rand_val2 = random_generator.rand(1);
+    shiftStart_to_lastRun_simple(j) = get_normal_dis_time(rand_val1,rand_val2,end_estimates_simple(1),end_estimates_simple(2));
+    %T1) calculates time when first run ends
+    rand_val = random_generator.rand(1);
+    shiftStart_to_firstRun_sim(j) = get_occurrence_time(rand_val,start_estimates_poisson(1),start_estimates_poisson(2));
+    first_runTime = shiftCycle(j,1) + shiftStart_to_firstRun_sim(j);
+    %T2) calculates time when last run ends
+    rand_val1 = random_generator.rand(1);
+    rand_val2 = random_generator.rand(1);
+    shiftStart_to_lastRun_sim(j) = get_normal_dis_time(rand_val1,rand_val2,end_estimates_gaussian(1),end_estimates_gaussian(2));
+    last_runTime = shiftCycle(j,1) + shiftStart_to_lastRun_sim(j);
+    %time : T2-T1
+    runSpan = last_runTime - first_runTime;
+    %number of runs per time(T2-T1)
+    rand_val = random_generator.rand(1);
+    n_runs = get_n_runs(rand_val,runSpan,n_runs_lambda);
+    %number of events per each run
+    if runSpan > 0 && n_runs >= 1
+        for i = 1:n_runs
+            rand_val = random_generator.rand(1);
+            eventTime = first_runTime + runSpan*rand_val;
+            rand_val = random_generator.rand(1);
+            n_Hbars = get_n_Hbars(rand_val,n_Hbar_lambda);
+            eventTimes(row_index:row_index+n_Hbars-1) = eventTime;
+            %Prepare for next iteration
+            row_index=row_index+n_Hbars;
+        end
     end
+    test_n_runs(j) = n_runs;
+    test_runSpan(j) = runSpan;
 end
 
 %Strip unused rows of preallocated array
-used_indices=event_times~=0;
-event_times=event_times(used_indices);
-n_left=0;
+used_indices=eventTimes~=0;
+eventTimes=eventTimes(used_indices);
+
+
+%-------------plot experimental & simulation data --------------------
+%plot time from when first run starts to when last run ends
+%hist
+f_first_hist = figure;
+x = (0:0.05:1);
+y1 = hist(shiftStart_to_firstRun,x); 
+y2 = hist(shiftStart_to_firstRun_sim,x);
+D = [y1;y2];
+bar(x,D',1.4);
+xlim([0 0.8]);
+xlabel('day')
+ylabel('count')
+legend('experiment','simulation')
+title('time from when first run starts to when last run ends');
+%empirical CDF
+f_first_plot = figure;
+[ydata, xdata] = ecdf(shiftStart_to_firstRun);
+plot(xdata,ydata,'b')
+hold on
+plot(xdata, 1 - exp( -start_estimates_poisson(2)*(xdata - start_estimates_poisson(1)) ),'r')
+ylim([0 1])
+xlabel('day')
+ylabel('Probability')
+legend('experiment','simulation')
+
+
+%plot time from when shift starts to when last run ends
+%hist
+f_end_hist = figure;
+x = (0:0.05:1);
+y1 = hist(shiftStart_to_lastRun,x); 
+y2 = hist(shiftStart_to_lastRun_sim,x);
+%y3 = hist(shiftStart_to_lastRun_simple,x);
+D = [y1;y2];%[y1;y2;y3];
+bar(x,D',1.4);
+xlim([0 0.8]);
+xlabel('day')
+ylabel('count')
+legend('experiment','simulation')
+title('time from when shift starts to when last run ends');
+%empirical CDF
+f_end_plot = figure;
+[y_ecdf, x_ecdf] = ecdf(shiftStart_to_lastRun);
+%cdfdata = normcdf(x,end_estimates_simple(1),end_estimates_simple(2));
+cdfdata_ls = normcdf(x,end_estimates_gaussian(1),end_estimates_gaussian(2));
+plot(x_ecdf,y_ecdf,'b')
+%hold on
+%plot(x,cdfdata,'g')
+hold on
+plot(x,cdfdata_ls,'r')
+xlim([0 0.8]);
+xlabel('day')
+ylabel('Probability')
+legend('experiment','estimates using least squares method')
+
+saveas(f_first_hist,'first_hist.pdf','pdf');
+saveas(f_first_plot,'first_plot.pdf','pdf');
+saveas(f_end_hist,'end_hist.pdf','pdf');
+saveas(f_end_plot,'end_plot.pdf','pdf');
+
+%---------for CHECK ---write total number of event % run,and run span------
+dispstr = ['total number of run is ',num2str(sum(test_n_runs)),' (experiment:320)'];
+disp(dispstr);
+dispstr = ['total run span is ',num2str(sum(test_runSpan)),' (experiment:34.8347)'];
+disp(dispstr);
+dispstr = ['total number of event time is ',num2str(numel(eventTimes)),' (experiment:386)'];
+disp(dispstr);
 
 end
 
-function [interarrival_time] = get_interarrival_time(rand_val,time_lambda)
-%Uses the Poisson interarrival time cdf and rand_val to pick an arrival
-interarrival_time=-log(1-rand_val)/time_lambda;
+function [time] = get_occurrence_time(rand_val,t_0,time_lambda)
+%Use poisson process and rand_val to pick a time. rand_val is the exclusive
+% event of the possibility not happening any event at specific time.
+time = - log(1 - rand_val)/time_lambda + t_0;
+end
+
+function [time] = get_normal_dis_time(rand_val1,rand_val2,mu,std)
+%Box-Muller transform. Needs two rands.
+rand_norm = sqrt(-2*log(rand_val1))*cos(2*pi*rand_val2);
+time = mu + std.*rand_norm;
 end
 
 function [n_Hbars] = get_n_Hbars(rand_val,n_Hbar_lambda)
 %Gives the number of Hbars in a given trapping run.
 
-% probability=@(n)n_Hbar_lambda^n*exp(-n_Hbar_lambda)/factorial(n);
-% n_Hbars=0;
-% running_probability=probability(n_Hbars);
-% while running_probability<rand_val
-%     n_Hbars=n_Hbars+1;
-%     running_probability=running_probability+probability(n_Hbars);
-% end
-
+%n_Hbars>=1
 probability=@(n)n_Hbar_lambda^n*exp(-n_Hbar_lambda)/factorial(n);
     %Probability of getting n events
 probability_0=probability(0); %Probability of getting 0 events
@@ -69,5 +201,40 @@ while running_probability<rand_val_scaled
     n_Hbars=n_Hbars+1;
     running_probability=running_probability+probability(n_Hbars);
 end
+end
 
+function[n_Runs] = get_n_runs(rand_val,timeSpan,lambda)
+%Decides number of events per the timeSpan in case of lambda
+%
+%rand_val : randumb number
+%timeSpan : time
+%lambda : mean of number of runs per unit time
+
+probability=@(n) (lambda*timeSpan)^n*exp(-lambda*timeSpan)/factorial(n);
+
+n=0;
+p_sum = probability(0);
+
+while p_sum < rand_val
+    n = n+1;
+    p_sum = p_sum + probability(n);
+end
+
+n_Runs = n;
+end
+
+function [mu] = lambda_event_number()
+ %Poisson estimate for number of events per run including about 651 +-12
+ % unsuccessful trials predicted. 386 events(320 runs) occured.
+    iterationNumber = 100000;
+    failed_runN = 651;
+    failed_runN_std = 12;
+    successful_eventN = 386; %old version:312 
+    successful_runN = 320; %old version:264
+    %eventN_0 = 651 + 12.*randn(iterationNumber,1);
+    %lambda = 312./(eventN_0 + 264);
+    eventN_0 = failed_runN + failed_runN_std.*randn(iterationNumber,1);
+    lambda = successful_eventN./(eventN_0 + successful_runN);
+    mu = mean(lambda);
+    %sigma = std(lambda);
 end
