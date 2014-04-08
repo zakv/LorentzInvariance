3089dbf8ac226f075a2180b0b4b1b70576259888
fumikac <fumika21@gmail.com>
2014-04-07 18:09:55 -0700

    changed to make a new function which generates even times with different method

---
+++ ExperimentalTimeData/Code/TimeAnalysisFunction/GenerateGuessedShiftTimeData_old.m
@@ -0,0 +1,30 @@
+%Reads Guessed Shift Time which was typed by hand on Excel file.
+
+oldDir = cd('../../DataSets/RawData/');
+readFilename = 'ShiftGuess.xlsx';
+readSheet = 1;
+readxlRange = 'C2:C114';
+shiftStTimeEx = xlsread(readFilename, readSheet, readxlRange);
+shiftStTime = x2mdate(shiftStTimeEx);
+
+readxlRange = 'D2:D114';
+shiftEndTimeEx = xlsread(readFilename, readSheet, readxlRange);
+shiftEndTime = x2mdate(shiftEndTimeEx);
+
+%Shift time only with successful events
+attemptedEventPerShift = calc_events_per_shift();
+successfulEventPerShift = attemptedEventPerShift(attemptedEventPerShift~=0);
+shiftStTimeEx_ev = shiftStTimeEx(attemptedEventPerShift~=0);
+shiftEndTimeEx_ev = shiftEndTimeEx(attemptedEventPerShift~=0);
+shiftStTime_ev = shiftStTime(attemptedEventPerShift~=0);
+shiftEndTime_ev = shiftEndTime(attemptedEventPerShift~=0);
+
+
+attemptedShiftTime = [shiftStTime, shiftEndTime];
+successfulShiftTime = [shiftStTime_ev, shiftEndTime_ev];
+cd(oldDir);
+
+oldDir = cd('../../DataSets/');
+save('GuessedShiftTimeData','attemptedShiftTime','attemptedEventPerShift',...
+    'successfulShiftTime','successfulEventPerShift');
+cd(oldDir);
