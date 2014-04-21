dad1bbdb02acbbab2b4b082b588d42eae4cf9201
zak <uphgreat@gmail.com>
2014-04-20 17:29:33 -0700

    Improved plot appearance on two tests
    CMB_Speed_Sinusoidal_Charge_Comparison.m and Test_Charman.m now
    plot the fit with a dash-dot line so that if it overlays the initial
    line, both plots can be seen.

--- SimulationData/Code/MiscellaneousScripts/CMB_Speed_Sinusoidal_Charge_Comparsion.m
+++ SimulationData/Code/MiscellaneousScripts/CMB_Speed_Sinusoidal_Charge_Comparsion.m
@@ -1,5 +1,12 @@
+%Plots Earth's CMB Speed and then plots the phase=0 sinusoidal charge
+%signal on the same graph.  Both are normalized and shifted to oscillate
+%around 0 for easy comparison.
+
 sun_cmb=0.0012338*299792458.0; %Taken form cmb_velocity.c
-times=linspace(datenum(2010,1,1),datenum(2012,1,1),500);
+sidereal_year=31558149.8/(60*60*24);  %Taken from signal_sine.m
+start_date=datenum(2010,1,1);
+times=linspace(start_date,start_date+sidereal_year,500); %Integer number of periods
+%So that substracting the mean centers the data around y=0
 temp_data_set.raw_data_set.left_date_times=times;
 temp_data_set.raw_data_set.right_date_times=[]; %Should also try swapping these
 speeds=datenum_to_cmb_velocity(times);
@@ -9,7 +16,10 @@ figure('WindowStyle','docked');
 speeds=speeds-mean(speeds);
 speeds=speeds/max(speeds); %Normalize speeds and charges for comparison
 charges=charges/max(charges);
-plot(times,speeds,times,charges);
+hold on
+plot(times,speeds,'b');
+plot(times,charges,'r-.');
+hold off
 ylim(1.02*[-1,1]);
 datetick('x','mmm yyyy');
 legend('Normalized CMB Speed','Normalized Charge');
--- SimulationData/Code/MiscellaneousScripts/Test_Charman.m
+++ SimulationData/Code/MiscellaneousScripts/Test_Charman.m
@@ -4,37 +4,41 @@
 
 %Parameters that may be worth adjusting
 amplitude=1.0;
-phase=0.2; %radians
+phase=0.36; %radians
 period=31558149.8/(60*60*24); %'year'
 n_points=500;
 
 %Code to produce plots
 omega=2*pi/period;
-times=linspace(datenum(2010,1,1),datenum(2012,3,14),n_points);
+start_date=datenum(2010,1,1);
+times=linspace(start_date,start_date+period,500); %Integer number of periods
 times=transpose(times);
 temp_data_set.raw_data_set.left_date_times=times;
 temp_data_set.raw_data_set.right_date_times=[];
 y_vals=signal_sine(temp_data_set,amplitude,period,phase);
 %y_vals=datenum_to_cmb_velocity(times); %Used for figuring out how to adjust phase from CharmanII/IV
+%y_vals=y_vals-mean(y_vals);
 
 A_1=CharmanIV(times,y_vals,period);
 fit_amplitude=abs(A_1);
 fit_phase=angle(A_1);
 fit_phase=fit_phase-pi/2; %Fits a cosine wave, this phase shifts it to a sine wave
-%fit_y_vals=fit_amplitude*sin(omega*times-fit_phase);
+
+fit_y_vals=fit_amplitude*sin(omega*times-fit_phase);
 %The following doesn't work due to the difference in phase reference for
 %the fit_phase and the given phase.  The given phase is relative to a sine
 %wave in phase with the CMB speed, while the returned phase is relative to
 %a cosine wave starting at datenum(0).
-fit_y_vals=signal_sine(temp_data_set,fit_amplitude,period,fit_phase);
+%fit_y_vals=signal_sine(temp_data_set,fit_amplitude,period,fit_phase);
 figure('WindowStyle','docked');
-plot(times,y_vals,times,fit_y_vals);
-datetick('x','mmm yyyy');
+hold on
+plot(times,y_vals,'b');
+plot(times,fit_y_vals,'r-.');
+hold off
+%datetick('x','mmm yyyy');
 legend('Input','Fit');
 legend('Location','SouthEast');
 amplitude_error=(fit_amplitude-amplitude)/amplitude;
 phase_error=(fit_phase-phase)/phase;
 fprintf('Amplitude fractional error: %0.2e\n',amplitude_error);
 fprintf('Phase fractional error: %0.2e\n',phase_error);
-
-
