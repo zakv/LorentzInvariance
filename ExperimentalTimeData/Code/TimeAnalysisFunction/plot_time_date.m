08f07c47da412e2c3bd304eec9a00976ff550fac
Fumikac <fumika21@gmail.com>
2014-04-26 13:40:17 -0700

    changed the input; you can select the color

--- ExperimentalTimeData/Code/TimeAnalysisFunction/plot_time_date.m
+++ ExperimentalTimeData/Code/TimeAnalysisFunction/plot_time_date.m
@@ -1,4 +1,4 @@
-function [] = plot_time_date(time)
+function [] = plot_time_date(time,color)
 %Plots clock time against date
 
 time = sort(time);
@@ -7,6 +7,6 @@ clockTime = H + MN./60 + S./3600.;
 
 %if you wanna plot from 23:00...
 %clockTime(H>=23) = clockTime(H>=23) - 24;
-
-plot(time, clockTime,'+');
+style = ['+',color];
+plot(time, clockTime,style);
 
