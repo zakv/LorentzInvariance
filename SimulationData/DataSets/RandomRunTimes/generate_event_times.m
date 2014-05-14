784a857601635d72bb7aedef1ae0c014f0f7090a
Fumikac <fumika21@gmail.com>
2014-05-14 15:02:36 -0700

    changed the method to choosing the number of events first, and assign the time later. Also change to use the time when last run starts to when shift ends

--- SimulationData/DataSets/RandomRunTimes/generate_event_times.m
+++ SimulationData/DataSets/RandomRunTimes/generate_event_times.m
@@ -1,280 +1,188 @@
-function [eventTimes,run_first_last,run_start_end] = generate_event_times(random_generator)
-%Returns a column vector of event times in UTC.
+function [eventTimes] = generate_event_times(random_generator)
+%for plotting time tables...[shiftCycle,operationCycle_sim,runCycle]
 
-%METHOD
-%**Here, "run" means the one which attempted to trap Hbars.
-%Decides time from when shift starts to when first run starts 
-%       - shifted exponential (fit the empirical CDF)
-%Decides time from when first run starts to when last run starts
-%       - normal distribution (fitting parameter:mean&std)
-%Decides number of successful run during the time span 
-%       - poissonian (with the restriction that it is greater than zero)
-%Decides time of successful run starts
-%       - uniform random
-%Decides number of events for each successful run
-%       - poisson process (fitting parameter:maximum likelihood estimate)
-%Decides time from when run starts to ends(assuming this is the event time)
-%       - normal distribution (fitting parameter:median%MAD)
+%Generates eventTimes
+% 1) decides number of runs - normal distributions ( sigma = rms )
+% & number of events oer run - maximum likelihood estimate, Poisson distribution
+% 2) decides when the first run starts and when the last run starts -exp
+% 3) randomly decides the event time
 
-%10000iterations
-%number of events : 388.6286+_29.0207 (experiment:386)
-%number of runs   : 322.1151+_23.1404 (experiment:320)
-%total run time   : 29.7177+_1.4017 (experiment:29.6966)
 
 %-------------get experimental data-------------
 oldDir = cd('../../../ExperimentalTimeData/Class');
 %gets time information
 successful = successful_run();
 attempted = attempted_run();
-shift = shift_cycle();
+shiftCycle = shift_cycle();
 
-successfulEventTime = successful.eventTime('utc','all','all');%utc
-shiftCycle = shift.attempted();
-attemtpedStartTime = attempted.startTime();
-%runTime = attempted.start2end_quench();
+successfulEventTime = successful.eventTime('utc','all','all');
+shiftCycle = shiftCycle.attempted();
+attemptedRunTime = attempted.startTime();
 cd(oldDir);
-
-%n_Hbars_experiment = vertcat(1*ones(261,1),2*ones(53,1),3*ones(5,1),4*ones(1,1));
-%this data is only for figure
 
 %-----------prepare data------------------------
 oldDir = cd('../../../ExperimentalTimeData/Code/TimeAnalysisFunction/');
-%change the time from local to utc
+%convert from local time to utc
 shiftCycle = st2utc_ch(shiftCycle);
-attemtpedStartTime = st2utc_ch(attemtpedStartTime);
+attemptedRunTime = st2utc_ch(attemptedRunTime);
 %get the first and last run in a shift
-[~,runsCycle] = calc_events_per_time_cycle(attemtpedStartTime,shiftCycle);
+[~,operationCycle] = calc_events_per_time_cycle(attemptedRunTime,shiftCycle);
 cd(oldDir);
 
-%shiftStart_to_firstRun = runsCycle(:,1) - shiftCycle(:,1);%for estimation and plot
-firstRun_to_lastRun = runsCycle(:,2) - runsCycle(:,1);
+n_runs_exp = numel(unique(successfulEventTime));%number of runs (experiment)
+n_cycle = length(operationCycle);%number of shift cycles
 
-totalRunSpan = sum(firstRun_to_lastRun);
-n_successfulRuns = numel(unique(successfulEventTime));%number of runs (experiment)
-n_cycle = length(runsCycle);%number of shift cycles
+%need for estimating parameters
+%shiftStart_to_firstRun = operationCycle(:,1) - shiftCycle(:,1);%for estimation and plot
+%lastRun_to_shiftEnd = shiftCycle(:,2) - operationCycle(:,2);
+%firstRun_to_lastRun = operationCycle(:,2) - operationCycle(:,1);
 
-%--------calculates estimetes parameter-------------
+%------------estimate parameters-------------
 %probability of getting one Hbar for the run - Poisson process
-lambda_n_Hbar=0.387532;%maximum likelihood estimate
+lambda_n_Hbars=0.387532;%maximum likelihood estimate
 
-%probability of getting successful run in a time
-lambda_n_runs=n_successfulRuns/totalRunSpan;
-
-%estimates time from when shift starts to when firstrun starts
+%probability of getting first run in a time
 start_estimates_poisson = [0.0058050, 9.3289];
-        %fit_shifted_exponential(shiftStart_to_firstRun);
-        
-%estimates time from when the first run starts to when last run starts using mean
-end_estimates = [3.4941, 0.066398];
-            %gamfit(firstRun_to_lastRun);
-%end_estimates_gaussian = [mean(firstRun_to_lastRun), std(firstRun_to_lastRun)];
+        %fit_shifted_poisson(shiftStart_to_firstRun);
+
+%estimates time from when last trial run starts to shift end
+params_exp = [-0.00932907, 41.7865];
+    %fit_shifted_exponential(lastRun_to_shiftEnd);
+    
 %time from when run starts to ends; exactly 10 min
 runStart2End = 10/60/24;
 
-%----------generate earliest&latest time you can start running----------
-eventTimes=zeros(500,1); %Preallocate
-row_index=1;
-jMax = n_cycle;
-run_index = 0;
-n_Hbars = zeros(1000,1);
+%--------1. decides number of runs--------------
+rms = sqrt(n_runs_exp);
+rand_val = random_generator.rand(1);
+n_runs_sim = round(get_normal_dis(rand_val, n_runs_exp, rms));
 
-test_n_runs = zeros(jMax,1);
-shiftStart_to_firstRun_sim = zeros(jMax,1);
-firstRun_to_lastRun_sim = zeros(jMax,1);
-first_runTime = zeros(jMax,1);
-last_runTime = zeros(jMax,1);
+%------2. decides operation cycle------------------
+%when first run ends and last run ends
+shiftStart_to_firstRun_sim = zeros(n_cycle,1);
+operationCycle_sim = zeros(n_cycle,2);
+firstRun_to_lastRun_sim = zeros(n_cycle,1);
 
-%for plot
-run_first_last = zeros(jMax,2);
-run_start_end = zeros(1000,2);
+for j = 1:n_cycle
+    %Calculates time when first trial run starts in the cycle
+    rand_val = random_generator.rand(1);
+    shiftStart_to_firstRun_sim(j) = get_shifted_exponential(rand_val,...
+        start_estimates_poisson(1),start_estimates_poisson(2));
+    operationCycle_sim(j,1) = shiftCycle(j,1) + shiftStart_to_firstRun_sim(j);
+    
+    %Calculates time when last trial run starts in the cycle
+    rand_val = random_generator.rand(1);
+    lastRun_to_shiftEnd_sim = get_shifted_exponential(rand_val,params_exp(1),params_exp(2));
+    operationCycle_sim(j,2) = shiftCycle(j,2) - lastRun_to_shiftEnd_sim;
+    
+    %time length from first to last trial run
+    firstRun_to_lastRun_sim(j) = operationCycle_sim(j,2) - operationCycle_sim(j,1);
 
-for j=1:jMax        
-        %T1) get time when first run starts
-        rand_val = random_generator.rand(1);
-        shiftStart_to_firstRun_sim(j) = get_occurrence_time(rand_val,...
-            start_estimates_poisson(1),start_estimates_poisson(2));
-        first_runTime(j) = shiftCycle(j,1) + shiftStart_to_firstRun_sim(j);
-        %dT) get time span from when first run starts to when last run starts
-        rand_val = random_generator.rand(1);
-        firstRun_to_lastRun_sim(j) = get_gamma_dis(rand_val,...
-            end_estimates(1),end_estimates(2));
-        last_runTime(j) = first_runTime(j) + firstRun_to_lastRun_sim(j);
-        if last_runTime(j) - shiftCycle(j,1) > 17/24
-            disp('warning: shift is longer than 17 hours');
-        end
-        if j~=jMax
-            if shiftCycle(j+1,1) -  last_runTime(j) < 0
-                disp('warning: run ends after next shift starts');
-            end
-        end
+    %if the time is negative, running time is 0(no possibility for getting
+    %successful events)
+    if firstRun_to_lastRun_sim(j) < 0
+        firstRun_to_lastRun_sim(j) = 0;
+        operationCycle_sim(j,:) = NaN;
+    end
 end
 
-%---------------------------Get run&event time!----------------------
-for j = 1:jMax        
-    %for plot
-    run_first_last(j,:) = [first_runTime(j), last_runTime(j)];
-    %get number of runs per the time span
-    rand_val = random_generator.rand(1);
-    n_runs = get_n_runs(rand_val,firstRun_to_lastRun_sim(j),lambda_n_runs);
-    
-    %get time and number of events per each run
-    if n_runs >= 1
-        runStartTime = zeros(n_runs,1);        
-        for i = 1:n_runs
-            run_index = run_index + 1;
-            %get time cycles where run can start.
-            possible_timeCycle = zeros(i+1,2);
-            possible_timeCycle(1,1) = first_runTime(j);
-            possible_timeCycle(i,2) = last_runTime(j);
-            
-            for k = 1:i-1
-                if runStartTime(k) - runStart2End < possible_timeCycle(k,1)
-                    possible_timeCycle(k,2) = possible_timeCycle(k,1);
-                else
-                possible_timeCycle(k,2) = runStartTime(k) - runStart2End;                    
+totalOperationTime_sim = sum(firstRun_to_lastRun_sim);
+
+%------------------------Get run&event time----------------------------
+%preallocate
+eventTimes = zeros(500,1);
+n_events_per_run = zeros(n_runs_sim,1);
+runStartTimes = zeros(n_runs_sim,1);
+row_index = 1;
+
+for m = 1:n_runs_sim
+    failed = 1;
+    while failed == 1
+        %decides time when run starts within the operation time (relative to the ope time)
+        rand_val = random_generator.rand(1);
+        runTime_rel = rand_val*totalOperationTime_sim;
+
+        %Calculates which cycle the runTime_rel is in
+        sum_operationTime = 0;
+        operationTime_sim = 0;
+        k = 0;
+        while runTime_rel > ( sum_operationTime + operationTime_sim )
+            k = k+1;        
+            sum_operationTime = sum_operationTime + operationTime_sim ;
+            operationTime_sim = firstRun_to_lastRun_sim(k);
+        end
+
+        %absolute time when run starts
+        runStartTimes(m) = operationCycle_sim(k,1) + runTime_rel - sum_operationTime;
+
+        %the new run starting time can't be +-runStart2End of other run
+        %starting time (no overlap)
+        failed = 0;
+        if m >=2
+            for n = 1:m-1
+                timeDiff = abs(runStartTimes(n) - runStartTimes(m));
+                if timeDiff < runStart2End && timeDiff > 0
+                    failed = 1;
+                    break
                 end
-                possible_timeCycle(k+1,1) = runStartTime(k) + runStart2End;
             end
-            if possible_timeCycle(i,1) > last_runTime(j)
-                possible_timeCycle(i,2) = possible_timeCycle(i,1);             
-            end
-            
-            if sum(possible_timeCycle(:,2)-possible_timeCycle(:,1))<0
-                disp('error: A run cannot be assigned to the too short shift.');
-            end
-            %get the time when run starts, but exclude the run spans
-            %that were already assigned and the time length of runSpan
-            %before when the next run starts.
-            rand_val = random_generator.rand(1);
-            runStartTime_scaled = sum(possible_timeCycle(:,2)-possible_timeCycle(:,1))*rand_val;  
-            possible_time = 0;
-            n_cycle = 0;
-            while runStartTime_scaled > possible_time
-               last_possible_time = possible_time;
-               n_cycle = n_cycle + 1;
-               possible_time = possible_time +...
-                   (possible_timeCycle(n_cycle,2) - possible_timeCycle(n_cycle,1));
-            end
-            
-            %Finally get time when run starts
-            runStartTime(i) = runStartTime_scaled - last_possible_time +...
-                possible_timeCycle(n_cycle,1);
-            %Gets eventTime
-            eventTime = runStartTime(i) + runStart2End;
-            
-            %for plot
-            run_start_end(run_index,:) = [runStartTime(i), eventTime];
-            
-            %sort the time from past to present
-            runStartTime = sort(runStartTime);
-            %put zeros at the end of the array
-            runStartTime = circshift(runStartTime,i - n_runs);
-            
-            %get number of events(>=1) per run
-            rand_val = random_generator.rand(1);
-            n_Hbars(run_index) = get_n_Hbars(rand_val,lambda_n_Hbar);
-            eventTimes(row_index:row_index+n_Hbars(run_index)-1) = eventTime;
-            %Prepare for next iteration
-            row_index=row_index+n_Hbars(run_index);
         end
     end
-    test_n_runs(j) = n_runs;
+        
+    %event time (run starting time + time from when run starts to ends)
+    eventTime = runStartTimes(m) + runStart2End;
+    
+    %number of events per run
+    rand_val = random_generator.rand(1);
+    n_events_per_run(m) = get_n_Hbars(rand_val,lambda_n_Hbars);
+    %reassign the array of event times
+    eventTimes(row_index:row_index+n_events_per_run(m)-1) = eventTime;
+    %Prepare for next iteration
+    row_index=row_index+n_events_per_run(m);
 end
 
-%Strip unused rows of preallocated array
-used_indices=eventTimes~=0;
-eventTimes=eventTimes(used_indices);
-%used_indices=n_Hbars~=0;
-%n_Hbars=n_Hbars(used_indices);%for plot
+eventTimes = eventTimes(eventTimes~=0);
+%for plotting 
+%n_events = sum(n_events_per_run);%for plotting
+%runCycle = [eventTimes - runStart2End, eventTimes];
 
-%for plot
-used_indices=run_start_end(:,1)~=0;
-run_start_end=run_start_end(used_indices,:);
 
 %{
-%-------------plot experimental & simulation data --------------------
-%plot time from when shift starts to when first run starts
-%hist
-figure
-x = (0:0.05:1);
-y1 = hist(shiftStart_to_firstRun,x); 
-y2 = hist(shiftStart_to_firstRun_sim,x);
-D = [y1;y2];
-bar(x,D',1.4);
-xlim([0 0.8]);
-xlabel('day')
-ylabel('count')
-legend('experiment','simulation')
-title('time from when shift starts to when first run starts');
+%---------for CHECK ---write total number of event % run,and run span------
+dispstr = ['total number of run is ',num2str(n_runs_sim),' (experiment:320)'];
+disp(dispstr);
+dispstr = ['total run span is ',num2str(sum(totalOperationTime_sim)),' (experiment:34.8347)'];
+disp(dispstr);
+dispstr = ['total number of event time is ',num2str(n_events),' (experiment:386)'];
+disp(dispstr);
+%}
 
-%plot time from when first run starts to when last run starts
-%hist
-figure
-x = (0:0.020:1);
-y1 = hist(firstRun_to_lastRun,x); 
-y2 = hist(firstRun_to_lastRun_sim,x);
-D = [y1;y2];
-bar(x,D',1.4);
-xlim([0 0.8]);
-xlabel('day')
-ylabel('count')
-legend('experiment','simulation')
-title('time from when first run starts to when last run starts');
-
-%plot number of Hbars per run
-figure
-x = (1:1:max(max(n_Hbars),4));
-y1 = hist(n_Hbars_experiment,x);
-y2 = hist(n_Hbars,x);
-D = [y1;y2];
-bar(x,D',1.4);
-xlabel('number of events per run');
-ylabel('count');
-legend('experiment','simulation');
-
-%plot events time diagram
-oldDir = cd('../../../ExperimentalTimeData/Code/TimeAnalysisFunction/');
+%{
+%----------------plot events time diagram-------------------
+oldDir = cd('../../../ExperimentalTimeData/Code/PlotFunction/');
 f_events_diagram = figure;
-plot_time_date(eventTimes,'r');
+plot_time_date(eventTimes,'b');
 set_for_time_graph();
 cd(oldDir);
-
 saveas(f_events_diagram,'eventTimes_diagram.pdf','pdf');
 %}
-%---------for CHECK ---write total number of event % run,and run span------
-dispstr = ['total number of run is ',num2str(sum(test_n_runs)),...
-' (experiment:',num2str(n_successfulRuns),')'];
-disp(dispstr);
-dispstr = ['total run span is ',num2str(sum(firstRun_to_lastRun_sim)),...
-' (experiment:',num2str(totalRunSpan),')'];
-disp(dispstr);
-dispstr = ['total number of event time is ',num2str(numel(eventTimes)),...
-' (experiment:',num2str(numel(successfulEventTime)),')'];
-disp(dispstr);
-
 end
 
-function [time] = get_occurrence_time(rand_val,t_0,time_lambda)
+function [time] = get_shifted_exponential(rand_val,t_0,time_lambda)
 %Use poisson process and rand_val to pick a time. rand_val is the exclusive
 % event of the possibility not happening any event at specific time.
 time = - log(1 - rand_val)/time_lambda + t_0;
 end
 
-%{
 function[value] = get_normal_dis(rand_val,mu,std)
 value = sqrt(2*std^2)*erfinv(2*rand_val - 1) + mu;
 end
-%}
 
-function [time] = get_gamma_dis(rand_val,k,theta)
-    time = theta*gammaincinv(rand_val,k);
-end
-
-function [n_Hbars] = get_n_Hbars(rand_val,lambda_n_Hbar)
+function [n_Hbars] = get_n_Hbars(rand_val,n_Hbar_lambda)
 %Gives the number of Hbars in a given trapping run.
 
 %n_Hbars>=1
-probability=@(n)lambda_n_Hbar^n*exp(-lambda_n_Hbar)/factorial(n);
+probability=@(n)n_Hbar_lambda^n*exp(-n_Hbar_lambda)/factorial(n);
     %Probability of getting n events
 probability_0=probability(0); %Probability of getting 0 events
 rand_val_scaled=(1-probability_0)*rand_val+probability_0;
@@ -286,43 +194,12 @@ while running_probability<rand_val_scaled
     running_probability=running_probability+probability(n_Hbars);
 end
 end
-
-function[n_Runs] = get_n_runs(rand_val,timeSpan,lambda)
-%Decides number of events per the timeSpan in case of lambda
-%
-%rand_val : randumb number
-%timeSpan : time
-%lambda : mean of number of runs per unit time
-
-probability=@(n) (lambda*timeSpan)^n*exp(-lambda*timeSpan)/factorial(n);
-
-n_Runs=0;
-p_sum = probability(0);
-
-while p_sum < rand_val
-    n_Runs = n_Runs+1;
-    p_sum = p_sum + probability(n_Runs);
-end
-end
-
+    
 %{
-function [estimates] = fit_shifted_exponential(data)
+function [estimates] = fit_shifted_poisson(data)
 %fits shifted poisson distribution by using probability function
 [ydata, xdata] = ecdf(data);
 estimates = fitcurve(xdata, ydata);
-
-%{
-%empirical CDF
-figure
-[ydata, xdata] = ecdf(data);
-plot(xdata,ydata,'b')
-hold on
-plot(xdata, 1 - exp( -estimates(2)*(xdata - estimates(1)) ),'r')
-ylim([0 1])
-xlabel('day')
-ylabel('Probability')
-legend('experiment','fitting')
-%}
 
     function [estimates] = fitcurve(xdata, ydata)
     % Call fminsearch with a random starting point.
